a88b1088a35cf2cd5b0451122304e65a5bb2e250
danielfms <danielfelipe@sirius.utp.edu.co>
2018-02-25 17:33:09 -0500

    update helpers: UnionFind, fast c++ i/o

--- helpers.cpp
+++ helpers.cpp
@@ -43,3 +43,40 @@ for (auto &[k, v] : trees){
     p = (v / n) * 100.0;
     cout << k << " " << fixed << setprecision(4) << p << endl; 
 }
+
+//speed cin, cout
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+//uniond find class
+class UnionFind{
+    private: 
+        vector<int> p;
+        vector<int> rank;
+    public:
+        UnionFind(int N){
+            rank.assign(N, 0);
+            p.assign(N,0);
+            for(int i = 0; i < N; i++){
+                p[i] = i;
+            }
+        }
+        int findSet(int i){
+            return p[i] == i ? i : (p[i] = findSet(p[i]));
+        }
+        bool isSameSet(int i, int j){
+            return findSet(i) == findSet(j);
+        }
+        void unionSet(int i, int j){
+            int x = findSet(i);
+            int y = findSet(j);
+            if(rank[x] > rank[y]){
+                p[y] = x;
+            }else{
+                p[x] = y;
+                if(rank[x] == rank[y]){
+                    rank[y]++;
+                }
+            }
+        }
+};
